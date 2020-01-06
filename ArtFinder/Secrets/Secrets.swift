70b4b54fd85808aa08e4e301eff2a97cfd99b1a0
Casandra <casandragrullon@pursuit.org>
2020-01-06 09:41:51 -0500

    added secrets folder

---
+++ ArtFinder/Secrets/Secrets.swift
@@ -0,0 +1,13 @@
+//
+//  Secrets.swift
+//  ArtFinder
+//
+//  Created by casandra grullon on 1/6/20.
+//  Copyright © 2020 casandra grullon. All rights reserved.
+//
+
+import Foundation
+
+struct Secrets {
+    
+}
